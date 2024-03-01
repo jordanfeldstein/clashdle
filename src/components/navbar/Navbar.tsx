c70e976d7f74ac197ce8505aaf710dd545dc9c3b
jordanfeldstein <83356267+jordanfeldstein@users.noreply.github.com>
2024-02-29 22:32:22 -0500

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -36,7 +36,7 @@ export const Navbar = ({
             />
           )}
         </div>
-        <p className="text-xl font-bold dark:text-white">{GAME_TITLE}</p>
+        <p className="text-xl font-bold dark:text-white"><a href="https://ibb.co/QJGZXVq"><img src="https://i.ibb.co/CwFqWyX/Clashdle-Logo.png" alt="Clashdle-Logo" border="0"></a></p>
         <div className="right-icons">
           <ChartBarIcon
             className="mr-3 h-6 w-6 cursor-pointer dark:stroke-white"
