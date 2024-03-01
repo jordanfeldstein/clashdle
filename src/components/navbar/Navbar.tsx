21f1ce9331ba737d6f8c4fdce9df5678ff496172
jordanfeldstein <83356267+jordanfeldstein@users.noreply.github.com>
2024-02-29 22:43:54 -0500

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -36,7 +36,7 @@ export const Navbar = ({
             />
           )}
         </div>
-        <p className="text-xl font-bold dark:text-white">{GAME TITLE} </p>
+        <p className="text-xl font-bold dark:text-white">{GAME_TITLE}</p>
         <div className="right-icons">
           <ChartBarIcon
             className="mr-3 h-6 w-6 cursor-pointer dark:stroke-white"
