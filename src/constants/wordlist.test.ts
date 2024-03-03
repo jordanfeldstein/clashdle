4e885672bab0a332106be393346c030eec845791
jordanfeldstein <83356267+jordanfeldstein@users.noreply.github.com>
2024-03-03 16:57:29 -0500

    Update wordlist.test.ts

--- src/constants/wordlist.test.ts
+++ src/constants/wordlist.test.ts
@@ -2,11 +2,6 @@ import { fail } from 'assert'
 
 import { WORDS } from './wordlist'
 
-// You may not want the list of solutions to be unique. In that case, disable this test
-describe('wordlist', () => {
-  test('words are unique', () => {
-    const uniqueWords = Array.from(new Set(WORDS))
-
     expect(WORDS.length).toEqual(uniqueWords.length)
 
     if (uniqueWords.length !== WORDS.length) {
